9baf8449b3e48606b8b2d421c76ebd06659dc600
Pedro Henrique Custódio Brito <pedropedroka130608@gmail.com>
2025-02-17 18:46:36 -0300

    Corrige o arquivo Main.java e adiciona o arquivo Teste1 usado para testar entrada e saída de dados no Java

--- aula01/Number.java
+++ aula01/Number.java
@@ -4,5 +4,6 @@ public class Number {
         System.out.println(15 - 5);
         System.out.println(5  * 2);
         System.out.println(20 / 2);
+        System.out.println("Hello World");
     }
 }
---
+++ aula01/Teste1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Teste1 {
+    public static void main0(String[] args) {
+        Scanner a = new Scanner(System.in); // Declara que a variável 'a' pode receber valor através do nextInt()
+        System.out.println("Insira um valor para A");
+        int n1 = a.nextInt(); // Lê um valor para a variável 'a'
+        Scanner b = new Scanner(System.in); // Declara que a variável 'b' pode receber valor através do nextInt()
+        System.out.println("Insira um valor para B");
+        int n2 = b.nextInt(); // Lê um valor para a variável 'b'
+
+        int n3 = n1 + n2; // Soma as variáveis
+
+        System.out.println("A soma de A e B é " + n3); // Exibe o resultado da soma
+    }
+}
