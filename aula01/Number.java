534ccd128771c499d7acc75fea63e0d9cdc4b095
Pedro Henrique Custodio <pedropedroka130608@gmail.com>
2025-02-17 09:18:46 -0300

    Aula sobre estrutura e códigos básicos em Java

---
+++ aula01/Number.java
@@ -0,0 +1,8 @@
+public class Number {
+    public static void main(String[] args) {
+        System.out.println(5  + 5);
+        System.out.println(15 - 5);
+        System.out.println(5  * 2);
+        System.out.println(20 / 2);
+    }
+}
