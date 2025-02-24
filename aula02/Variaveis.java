ed1f9f07807570dab9f1f1247e534ca5c7f8da17
Pedro Henrique Custódio <pedropedroka130608@gmail.com>
2025-02-24 09:30:57 -0300

    Aula sobre variáveis no Java

---
+++ aula02/Variaveis.java
@@ -0,0 +1,34 @@
+public class Variaveis {
+
+    public static void main(String[] args) {
+        String S1 = "Olá!";
+        String S2 = " Tudo bem?";
+
+        System.out.println(S1);
+        System.out.println(S1 + S2);
+
+        int i1 = 3;
+        int i2 = 4;
+
+        System.out.println(i1);
+        System.out.println(i1 + i2);
+        
+        float f1 = 3.14159f;
+        float f2 = 2.5f;
+
+        System.out.println(f1);
+        System.out.println(f1 + f2);
+        
+        char c1 = 'A';
+        char c2 = 2;
+
+        System.out.println(c1);
+        System.out.println(c1 + c2);
+        
+        boolean b1 = false;
+        boolean b2 = true;
+        
+        System.out.println(b1);
+        System.out.println(b2);
+    }
+}
