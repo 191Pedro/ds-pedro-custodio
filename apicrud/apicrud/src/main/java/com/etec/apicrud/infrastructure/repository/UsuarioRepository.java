56ce511d06f2575aa2e5ee042a0772734c7c15ad
Pedro Custódio <pedropedroka130608@gmail.com>
2025-10-06 09:20:01 -0300

    Oi

---
+++ apicrud/apicrud/src/main/java/com/etec/apicrud/infrastructure/repository/UsuarioRepository.java
@@ -0,0 +1,15 @@
+package com.etec.apicrud.infrastructure.repository;
+
+import com.etec.apicrud.infrastructure.entities.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    Optional<Usuario> findByEmail(String email);
+
+    @Transactional
+    void deleteByEmail(String email);
+}
